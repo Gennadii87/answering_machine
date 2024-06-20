7434eab6b04ba9b2b8cdc3c5f3f0e9c4c9723339
Gennadii87 <Gennadii_s@inbox.ru>
2024-06-21 07:12:18 +1000

    update code

--- database/config.py
+++ database/config.py
@@ -8,5 +8,6 @@ POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_DB = os.getenv('POSTGRES_DB')
 POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+PORT = os.getenv('PORT')
 
-conn_url = f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}'
+conn_url = f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{PORT}/{POSTGRES_DB}'
